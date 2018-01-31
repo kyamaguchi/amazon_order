db763055c5b8be3994c736fc45e9b897d5617c18
John Randall <john@johnrandall.com>
2018-01-31 01:04:08 -0500

    Bugfix: Show all products with multiple shipments
    Order has shipment, which has products

--- spec/amazon_order/parsers/order_spec.rb
+++ spec/amazon_order/parsers/order_spec.rb
@@ -1,58 +1,137 @@
 require 'spec_helper'
 
-describe AmazonOrder::Parsers::Order do
-  TARGET_DIR = ENV['ORDERS_DIR'].presence || 'spec/fixtures/files'
-
-  def ensure_fixture_filepath(path)
-    pending("Put your html in #{path} for testing") unless path && File.exists?(path)
-    path
+RSpec.shared_examples "generic order specs" do
+  describe '#order number' do
+    it 'has a number ' do
+      expect(order.order_number).to match(/\A[D\d][\d\-]+\z/)
+    end
+  end
+  it 'has information' do
+    expect(order.order_placed).to be_a(Date)
+    expect(order.order_total).to be_a(Numeric)
+    expect(order.order_total.to_s).to match(/\A[\d\.]+\z/)
+    expect(order.order_details_path).to match(%r{\A/gp/})
+    expect(order.fetched_at).to be_present
   end
 
-  Dir.glob("#{TARGET_DIR}/*html").each do |filepath|
-    context "with file (#{filepath})" do
-      parser = AmazonOrder::Parser.new(filepath)
-      before do
-        ensure_fixture_filepath(filepath)
+  describe '#to_json' do
+    it 'returns json' do
+      json = order.to_json
+      expect(json).to match(/"order_total"/)
+      expect(json).to match(/"products"/)
+      expect(json).to match(/"\d{4}-\d{2}-\d{2}"/) # date
+      expect(parser.orders.to_json).to match(/\[{"order_placed":/)
+    end
+  end
+
+  describe '#shipments' do
+    it 'does not error' do
+      expect { order.shipments }.not_to raise_error
+    end
+  end
+
+  describe '#shipment_products' do
+    it 'does not error' do
+      expect { order.shipment_products.count }.not_to raise_error
+    end
+  end
+
+  describe '#digital_products' do
+    it 'does not error' do
+      expect { order.digital_products.count }.not_to raise_error
+    end
+  end
+end
+
+def ensure_fixture_filepath(path)
+  pending("Put your html in #{path} for testing") unless path && File.exists?(path)
+  path
+end
+
+
+describe AmazonOrder::Parsers::Order do
+  let(:parser) { AmazonOrder::Parser.new(filepath) }
+  let(:order) { parser.orders[index_of_order] }
+
+  context 'user fixtures' do
+    TARGET_DIR = ENV['ORDERS_DIR'].presence || 'spec/fixtures/files'
+    Dir.glob("#{TARGET_DIR}/*html").each do |filepath|
+      context "with file (#{filepath})" do
+        let(:filepath) { filepath }
+        before do
+          ensure_fixture_filepath(filepath)
+        end
+        AmazonOrder::Parser.new(filepath).orders.each_index do |i|
+          context "order at index #{i}" do
+            let(:index_of_order) { i }
+            include_examples 'generic order specs'
+          end
+        end
       end
+    end
+  end
 
-      parser.orders.each do |order|
-        current_index = parser.orders.index(order)
+  context 'specific fixtures' do
+    context 'order with digital deliveries' do
+      let(:filepath) { 'spec/fixtures/files/order-amazon_com2017-contains-digital.html' }
+      let(:index_of_order) { 0 }
 
-        context "for order at #{current_index}" do
 
-          describe "#order number" do
-            it "#{order.order_number}" do
-              expect(order.order_number).to match(/\A[D\d][\d\-]+\z/)
-            end
-          end
+      describe '#shipments' do
+        it 'has no shipments' do
+          expect(order.shipments.count).to eq 0
+        end
+      end
+      describe '#shipment_products' do
+        it 'has no shipment products' do
+          expect(order.shipment_products.count).to eq 0
+        end
+      end
+      describe '#digital_products' do
+        it 'has a digital product' do
+          expect(order.digital_products.count).to eq 1
+        end
+      end
+    end
 
-          it "has information" do
-            expect(order.order_placed).to be_a(Date)
-            expect(order.order_total).to be_a(Numeric)
-            expect(order.order_total.to_s).to match(/\A[\d\.]+\z/)
-            expect(order.order_details_path).to match(%r{\A/gp/})
-            expect(order.fetched_at).to be_present
-            expect(order.products.size).to be > 0
-          end
+    context 'order with with home services' do
+      let(:filepath) { 'spec/fixtures/files/order-2018-p1-20180129141532-contains-home-service.html' }
+      let(:index_of_order) { 9 }
+      include_examples 'generic order specs'
+      describe '#shipments' do
+        it 'has a shipments' do
+          expect(order.shipments.count).to eq 1
+        end
+      end
+      describe '#shipment_products' do
+        it 'has a shipment product' do
+          expect(order.shipment_products.count).to eq 1
+        end
+      end
+      describe '#digital_products' do
+        it 'has no digital products' do
+          expect(order.digital_products.count).to eq 0
+        end
+      end
+    end
 
-          describe "#shipment_note" do
-            context 'with a service order' do
-              next unless order.order_number == '114-2295903-7028239'
-              it 'returns nil' do
-                expect(order.shipment_note).to be_nil
-              end
-            end
-          end
-
-          describe "#to_json" do
-            it 'returns json' do
-              json = order.to_json
-              expect(json).to match(/"order_total"/)
-              expect(json).to match(/"products"/)
-              expect(json).to match(/"\d{4}-\d{2}-\d{2}"/) # date
-              expect(parser.orders.to_json).to match(/\[{"order_placed":/)
-            end
-          end
+    context 'order with with physical shipments' do
+      let(:filepath) { 'spec/fixtures/files/order-2018-p1-20180129141532-contains-home-service.html' }
+      let(:index_of_order) { 2 }
+      include_examples 'generic order specs'
+      describe '#shipments' do
+        it 'has 3 shipments' do
+          expect(order.shipments.count).to eq 3
+        end
+      end
+      describe '#shipment_products' do
+        it 'has 5 shipment products' do
+          expect(order.shipment_products.count).to eq 5
+        end
+      end
+      describe '#digital_products' do
+        it 'has no digital products' do
+          expect(order.digital_products.count).to eq 0
         end
       end
     end
--- spec/amazon_order/parsers/shipment_spec.rb
+++ spec/amazon_order/parsers/shipment_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe AmazonOrder::Parsers::Product do
+describe AmazonOrder::Parsers::Shipment do
   TARGET_DIR = ENV['ORDERS_DIR'].presence || 'spec/fixtures/files'
 
   def ensure_fixture_filepath(path)
@@ -19,19 +19,18 @@ describe AmazonOrder::Parsers::Product do
           order.shipments.each do |shipment|
             current_index = order.shipments.index(shipment)
             context "for shipment at #{current_index}" do
-
-              shipment.products.each do |product|
-                current_index = shipment.products.index(product)
-                context "for product at #{current_index}" do
-
-                  it 'has information' do
-                    expect(product.title).to be_present
-                    expect(product.path).to match(%r{\A/gp/product/})
-                    expect(product.content).to be_present
-                    expect(product.image_url).to match(%r{/images/I/[^.]+\.jpg})
-                    expect(product.fetched_at).to be_present
+              describe '#shipment_note' do
+                context 'with a service order' do
+                  next unless order.order_number == '114-2295903-7028239'
+                  it 'returns nil' do
+                    expect(shipment.shipment_note).to be_nil
                   end
+                end
+              end
 
+              describe '#products' do
+                it 'has products' do
+                  expect(shipment.products.size).to be > 0
                 end
               end
             end
@@ -41,4 +40,3 @@ describe AmazonOrder::Parsers::Product do
     end
   end
 end
-
