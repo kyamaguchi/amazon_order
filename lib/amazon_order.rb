ed278e170243b378ff132a0a3d56d2ca16b963b7
Kazuho Yamaguchi <kzh.yap@gmail.com>
2023-07-23 19:35:50 +0900

    Remove webdrivers gem
    https://github.com/titusfortner/webdrivers/issues/247

--- lib/amazon_order.rb
+++ lib/amazon_order.rb
@@ -7,7 +7,6 @@ require "amazon_order/parsers/shipment"
 require "amazon_order/parsers/product"
 require "amazon_order/parser"
 require "amazon_order/writer"
-require 'webdrivers'
 
 module AmazonOrder
 end
